053191764af11089d3a22fbead9d7a697eca3496
charan <sravanamcharan20@gmail.com>
2024-09-07 11:43:52 +0530

    changed donor controller

--- backend/controllers/donor.controller.js
+++ backend/controllers/donor.controller.js
@@ -3,24 +3,14 @@ import User from  '../models/user.model.js'
 
 export const donationform = async (req, res) => {
   try {
-    
     const donoremail = req.body.email;
-
-    // Check if a donor with the same email already exists
     const existingUser = await User.findOne({ email: donoremail });
     if(!existingUser) 
       return res.status(400).json({ message: 'Email is not there please sign up' });
-
     const donor = new Donor(req.body);
-
-    // Save the new donor to the database
     const savedDonor = await donor.save();
-
-
-    // Respond with the saved donor data
     res.status(201).json(savedDonor);
   } catch (error) {
-    // Handle any errors that occur during the process
     res.status(500).json({ message: error.message });
   }
 };
