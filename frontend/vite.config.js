2fbb0c9195cbf62fa20d5663b1ab4fad221527a7
charan <sravanamcharan20@gmail.com>
2024-09-14 09:48:08 +0530

    host changed to universal

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '172.18.6.55',
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://localhost:6001',
