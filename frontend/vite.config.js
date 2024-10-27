7d7ec731e7859cccda08237abcb37d2c101faf3b
charan <sravanamcharan20@gmail.com>
2024-10-27 16:34:57 +0530

    c

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
-  const isDevelopment = mode === 'development';
-
-  return {
-    server: {
-      host: '0.0.0.0',
-      ...(isDevelopment && {
-        proxy: {
-          '/api': {
-            target: 'http://localhost:6001',
-            secure: false,
-            changeOrigin: true,
-          },
-        },
-      }),
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:6001',
+        secure: false,
+      },
     },
-    build: {
-      outDir: 'dist',
-    },
-    plugins: [react()],
-  };
+  },
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [react()],
 });
